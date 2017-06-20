a4a1300609157e9fca4293270b85fa7745eb712a
Nomsy <me@nomsy.net>
2017-06-19 23:31:02 -0400

    Fixes to constants and compilers

--- src/commands/utils/about.ts
+++ src/commands/utils/about.ts
@@ -1,6 +1,7 @@
 import { Client, Command, Message, Time } from 'yamdbf';
 import { RichEmbed, Guild } from 'discord.js';
 import { EmbedCode } from '../../lib/Util';
+import * as os from 'os';
 
 const { version } = require('../../../package');
 
@@ -23,14 +24,17 @@ export default class extends Command<Client> {
             .setColor(EmbedCode.Profile)
             .setDescription(`Ankh v${version} is a multi-purpose WIP Discord bot.`)
             .addField('Servers', this.client.guilds.size.toString(), true)
-            .addField('Channels', this.client.channels.size.toString())
+            .addField('Channels', this.client.channels.size.toString(), true)
             .addField('Users', this.client.guilds.map((guild: Guild) => guild.memberCount)
                 .reduce((memA: number, memB: number) => memA + memB), true)
             .addField('Memory Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`, true)
-            .addField('Help', `To see currently avaliable commands, type ${calltag}@${this.client.user.tag} help${calltag}.`, true)
+            .addField('Uptime', Time.difference(this.client.uptime * 2, this.client.uptime).toString(), true)
+            .addField('Kernel', `${os.platform()} v${os.release()}`, true)
+            .addBlankField()
             .addField('More information to follow', 'As Ankh is currently under development, expect more to be added here.', true)
+            .addField('Help', `To see currently avaliable commands, type ${calltag}@${this.client.user.tag} help${calltag}.`, true)
             .setTimestamp();
 
-        message.channel.sendEmbed(embed);
+        message.channel.send({embed: embed, disableEveryone: true});
     }
 }
--- src/lib/Util.ts
+++ src/lib/Util.ts
@@ -1,27 +1,13 @@
 import { AnkhClient } from './AnkhClient';
 
 /**
- * This constains different types of embed colors Ankh
- * uses in any command using a RichEmbed.
- */
-export enum ColorConstant {
-    'Default' = 0x000000,
-    'Green' = 0x2ECC71,
-    'DarkGreen' = 0x1F8B4C,
-    'Purple' = 0x9B59B6,
-    'DarkPurple' = 0x71368A,
-    'Orange' = 0xE67E22,
-    'Red' = 0xE74C3C,
-    'DarkRed' = 0x992D22
-}
-
-/**
 * This is embed colors associated with embeds and
 * used color constants within Ankh commands.
 */
 export enum EmbedCode {
-    'Error' = ColorConstant.Red,
-    'Warn' = ColorConstant.Orange,
-    'Info' = ColorConstant.Green,
-    'Profile' = ColorConstant.Purple
+    'Default' = 0x000000,
+    'Error' = 0xE74C3C,
+    'Warn' = 0xE67E22,
+    'Info' = 0x2ECC71,
+    'Profile' = 0x9B59B6
 }
