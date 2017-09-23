983111d2b2e1baca4f4f48a0d3599b8644b492bb
Truency <me@nomsy.net>
2017-09-23 01:39:23 -0400

    Added sequelize

---
+++ src/database/Database.ts
@@ -0,0 +1,41 @@
+import * as Sequelize from 'sequelize';
+import { Logger } from 'yamdbf';
+
+export class Database {
+
+    private db: any;
+    private readonly logger: Logger = Logger.instance();
+
+    public constructor(creds: any) {
+        this.db = new Sequelize(creds.database, creds.user, creds.password, {
+            dialect: 'postgres',
+            logging: false,
+            pool: {
+                max: 10,
+                min: 1
+            },
+            dialectOptions: { keepAlive: true }
+        });
+    }
+
+    public static get db(): any {
+        return this.db;
+    }
+
+    public init(): Promise<any> {
+        return this.db.authenticate()
+            .then((): Promise<void> => this.logger.info('postgres', 'Connection established successfully.'))
+            .then((): Promise<void> => this.logger.info('postgres', 'Syncing...'))
+            .then((): void => {
+                this.db.sync()
+                    .then((): void => {
+                        this.logger.info('postgres', 'Sync successful!');
+                    })
+                    .catch((err: Error): void => {
+                        this.logger.error('postgres', `Sync unsuccessful: ${err}`);
+                    });
+            }).catch((err: Error): void => {
+                this.logger.error('postgres', `Error connecting to database: ${err}`);
+            });
+    }
+}
