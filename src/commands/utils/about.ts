2bdc26fd136cbf2c61543a58ff8147fa9f0bd622
truency <me@nomsy.net>
2017-10-26 11:55:54 -0400

    Fix about command

--- src/commands/utils/about.ts
+++ src/commands/utils/about.ts
@@ -1,6 +1,6 @@
 import { Client, Command, Message, Time } from 'yamdbf';
 import { RichEmbed, Guild } from 'discord.js';
-import { EmbedCode, channelOnly } from '../../lib/Util';
+import { EmbedCode } from '../../lib/Util';
 
 const { version } = require('../../../package');
 
