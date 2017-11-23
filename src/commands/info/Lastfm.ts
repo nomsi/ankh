6b03bc62e6336c9b5f8edd8e7fb82c4e6cb40b0b
truency <me@nomsy.net>
2017-11-23 17:50:19 -0500

    whoops

--- src/commands/info/Lastfm.ts
+++ src/commands/info/Lastfm.ts
@@ -2,10 +2,14 @@ import { CommandDecorators as c, Client, Command, Message } from 'yamdbf';
 import { RichEmbed } from 'discord.js';
 import * as request from 'snekfetch';
 
+const { LASTFM_API } = process.env;
+
 @c.aliases('lastfm', 'np', 'lfm')
 @c.info('Last.fm command')
 @c.group('ankh-info')
 export class Lastfm extends Command<Client> {
+
+
     public constructor() {
         super({
             name: 'lastfm',
@@ -24,6 +28,7 @@ export class Lastfm extends Command<Client> {
      * @returns {object} embed object
      */
     private lastfm(username: string): object {
+        const uri = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${LASTFM_API}&format=json`;
         return {};
     }
 }
