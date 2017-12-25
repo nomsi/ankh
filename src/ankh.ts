b0b69fe5207bdfde5c4e926be87f2a4987ea123b
truency <me@nomsy.net>
2017-12-25 01:12:05 -0500

    *poof*

--- src/ankh.ts
+++
@@ -1,8 +0,0 @@
-import { AnkhClient } from './lib/AnkhClient';
-const client: AnkhClient = new AnkhClient();
-
-client.start();
-client.on('disconnect', (): void => process.exit(0));
-process.on('unhandledRejection', (reason: string): void => {
-    console.error(reason);
-});
