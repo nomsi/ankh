a78aec54a72ee39c134334965bd622064a3cd744
Nomsy <bpalmer647@gmail.com>
2018-04-20 00:08:50 -0400

    Update gulpfile.js
    fixed. removed stupid deps.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const tsc = require('gulp-tsc');
 const tslint = require('tslint');
 const lint = require('gulp-tslint');
 const del = require('del');
-const shell = require('gulp-shell');
 
 const project = ts.createProject('tsconfig.json');
 const linter = tslint.Linter.createProgram('tsconfig.json');
@@ -21,12 +20,7 @@ gulp.task('lint', () => {
 
 gulp.task('build', () => {
     del.sync(['./dist/**/*.*']);
-    const tsCompile = gulp.src('./src/**/*.ts')
-        .pipe(project());
+    const tsCompile = gulp.src('./src/**/*.ts').pipe(project());
     tsCompile.pipe(gulp.dest('dist/'));
-    gulp.src('./src/**/*.js').pipe(gulp.dest('dist/'));
-    gulp.src('./src/**.json').pipe(gulp.dest('dist/'));
     return tsCompile.js.pipe(gulp.dest('dist/'));
 });
-
-gulp.task('run', shell.task(['node ./dist/ankh.js']));
